31defbae19665bd5270c57cb6fa1d630c7e23060
Khawlamhb <mouhoubi.khawla@gmail.com>
2019-03-03 11:14:57 +0100

    exo3

---
+++ src/ex3/Boulangerie.java
@@ -0,0 +1,49 @@
+public class Boulangerie implements Lock{
+
+	private boolean flag[];
+	private int noms[];
+	
+	int nom;
+	
+	public Boulangerie(int nbThread) {
+		flag = new boolean[nbThread];
+		noms = new int[nbThread];
+		
+		for(int i=0; i<nbThread; i++) {
+			flag[i] = false;
+			noms[i] = 0;
+		}
+	}
+	
+	private int maxNoms() {
+		int max = noms[0];
+		for(int i=1;i<noms.length;i++) {
+			if(max < noms[i]) max = noms[i];
+		}
+		return max;
+	}
+	
+	private boolean otherThreadCritic() {
+		for(int i=0;i<flag.length;i++) {
+			if(i !=nom && flag[i] && noms[i] < nom) return true;
+		}
+		return false;
+	}
+	@Override
+	public void lock() {
+		flag[nom] = true;
+		noms[nom] = maxNoms() + 1;
+		while(otherThreadCritic()) {
+			System.out.println("Thread numéro "+ nom + " attend.");
+		}
+		
+		
+	}
+
+	@Override
+	public void unlock() {
+		flag[nom] = false;
+		
+	}
+
+}
---
+++ src/ex3/Peterson.java
@@ -0,0 +1,37 @@
+public class Peterson implements Lock{
+	int nom;
+	
+	private boolean[] flag = {false, false};
+	private volatile int turn;
+	
+	public Peterson(int nom) {
+		this.nom = nom;
+	}
+	
+	public int otherThread() {
+		if(nom == 1) return 0;
+		return 1;
+	}
+	
+	@Override
+	public void lock(){
+        flag[nom] = true;
+        turn = otherThread();
+        
+        while(flag[otherThread()] && turn == otherThread()){
+        	System.out.println("Thread number "+ nom + " is waiting.");
+        };
+        
+        System.out.println("Thread number "+ nom + " is in the critical section");
+    }
+	
+	@Override
+	public void unlock(){
+       flag[nom] = false;
+    }
+	
+	public static void main(String[] args) {
+	   Peterson p1 = new Peterson(0);
+	   Peterson p2 = new Peterson(1);
+   }
+ }
